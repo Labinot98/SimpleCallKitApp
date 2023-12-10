380f5085c87d0fc8c4f02f91acbac80ca5ff9d88
Labinot Pajaziti <38728474+Labinot98@users.noreply.github.com>
2023-12-10 12:31:45 +0100

    Initial Commit

---
+++ SimpleCallKitApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleCallKitApp
+//
+//  Created by Pajaziti Labinot on 10.12.23..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
