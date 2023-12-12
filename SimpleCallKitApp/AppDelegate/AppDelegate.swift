785dc77a336bb729e1b7e7b0891f25adba65b9c1
Labinot Pajaziti <38728474+Labinot98@users.noreply.github.com>
2023-12-12 12:54:55 +0100

    Recive call if phone are locked

--- SimpleCallKitApp/AppDelegate/AppDelegate.swift
+++ SimpleCallKitApp/AppDelegate/AppDelegate.swift
@@ -21,8 +21,34 @@ class AppDelegate: UIResponder, UIApplicationDelegate, PKPushRegistryDelegate {
         voipRegistry.desiredPushTypes = [.voIP]
         
         callManager = DefaultCallManager()
+        
+        // Call the function to simulate incoming call after a delay
+        scheduleIncomingVoIPHandlingAfterDelay()
+        
         return true
     }
+    // Function to simulate handling incoming VoIP call after a delay
+      func scheduleIncomingVoIPHandlingAfterDelay() {
+          // Simulate handling incoming call after 5 seconds (adjust the delay as needed)
+          DispatchQueue.main.asyncAfter(deadline: .now() + 5.0) {
+              self.handleIncomingVoIPCall()
+          }
+      }
+
+      // Function to simulate handling of an incoming VoIP call
+      func handleIncomingVoIPCall() {
+          // Simulate incoming call handling using your CallKit logic
+          // For example:
+          let update = CXCallUpdate()
+          update.remoteHandle = CXHandle(type: .generic, value: "Caller Name")
+          update.hasVideo = false
+
+          callManager.reportIncomingCall(with: UUID(), update: update) { success in
+              if !success {
+                  // Handle failure to report incoming call
+              }
+          }
+      }
 
     // Handle incoming calls using CallKit
     func pushRegistry(_ registry: PKPushRegistry, didReceiveIncomingPushWith payload: PKPushPayload, for type: PKPushType) {
@@ -41,6 +67,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, PKPushRegistryDelegate {
             }
         }
     }
+    
 
     // Implement other PKPushRegistryDelegate methods if necessary
 
