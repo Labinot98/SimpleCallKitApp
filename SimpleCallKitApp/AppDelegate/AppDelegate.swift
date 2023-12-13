e590d5ef53651bff56c5bb9928874443f3b0ccf5
Labinot Pajaziti <38728474+Labinot98@users.noreply.github.com>
2023-12-13 22:54:47 +0100

    refact somthing

--- SimpleCallKitApp/AppDelegate/AppDelegate.swift
+++ SimpleCallKitApp/AppDelegate/AppDelegate.swift
@@ -19,37 +19,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate, PKPushRegistryDelegate {
         voipRegistry = PKPushRegistry(queue: DispatchQueue.main)
         voipRegistry.delegate = self
         voipRegistry.desiredPushTypes = [.voIP]
-        
         callManager = DefaultCallManager()
-        
-        // Call the function to simulate incoming call after a delay
-        scheduleIncomingVoIPHandlingAfterDelay()
         
         return true
     }
-    // Function to simulate handling incoming VoIP call after a delay
-      func scheduleIncomingVoIPHandlingAfterDelay() {
-          // Simulate handling incoming call after 5 seconds (adjust the delay as needed)
-          DispatchQueue.main.asyncAfter(deadline: .now() + 5.0) {
-              self.handleIncomingVoIPCall()
-          }
-      }
-
-      // Function to simulate handling of an incoming VoIP call
-      func handleIncomingVoIPCall() {
-          // Simulate incoming call handling using your CallKit logic
-          // For example:
-          let update = CXCallUpdate()
-          update.remoteHandle = CXHandle(type: .generic, value: "Caller Name")
-          update.hasVideo = false
-
-          callManager.reportIncomingCall(with: UUID(), update: update) { success in
-              if !success {
-                  // Handle failure to report incoming call
-              }
-          }
-      }
-
     // Handle incoming calls using CallKit
     func pushRegistry(_ registry: PKPushRegistry, didReceiveIncomingPushWith payload: PKPushPayload, for type: PKPushType) {
         guard type == .voIP, let callUUIDString = payload.dictionaryPayload["callUUID"] as? String,
@@ -67,14 +40,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate, PKPushRegistryDelegate {
             }
         }
     }
-    
-
-    // Implement other PKPushRegistryDelegate methods if necessary
-
-
-    func pushRegistry(_ registry: PKPushRegistry, didUpdate pushCredentials: PKPushCredentials, for type: PKPushType) {
-        // Handle push token registration, if needed
-        print(pushCredentials.token)
+    // Call this function when the app receives push credentials
+    func pushRegistry(_ voipRegistry: PKPushRegistry, didUpdate pushCredentials: PKPushCredentials, for type: PKPushType) {
+        // Display the iOS device token in the Xcode console
+        print(pushCredentials.token.map { String(format: "%02.2hhx", $0) }.joined())
     }
 
     func pushRegistry(_ registry: PKPushRegistry, didInvalidatePushTokenFor type: PKPushType) {
